4e6e43857736b972c0ecfcf53cd478064a674957
Mariya <mary.egorovva@yandex.by>
2023-02-27 16:15:27 +0300

    Change slideLeft

--- Slider/index.js
+++ Slider/index.js
@@ -7,12 +7,23 @@ let activeSlideIndex = 0;
 upButton.addEventListener("click", () => changeSlide());
 
 const changeSlide = () => {
+
   const slideHeight = slideRight.children[0].clientHeight;
+  const slideHeight2 = slideLeft.children[0].clientHeight;
 
   let currentMarginTop = Number(slideRight.style.marginTop.slice(0, -2));
+  let currentMarginTop2 = Number(slideLeft.style.marginTop.slice(0, -2));
+
   if (currentMarginTop === -slideHeight * (slideRight.children.length - 1)) {
     slideRight.style.marginTop = "0px";
   } else {
     slideRight.style.marginTop = currentMarginTop - Number(slideHeight) + "px";
+  }
+
+  
+  if (currentMarginTop2 === -slideHeight2 * (slideLeft.children.length - 1)) {
+    slideLeft.style.marginTop = "0px";
+  } else {
+    slideLeft.style.marginTop = currentMarginTop2 - Number(slideHeight2) + "px";
   }
 };
